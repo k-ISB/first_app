84a302e8abf7ee2844fe64bcc0f8d1fe628862fc
k-ISB <egoukister1004@outlook.com>
2019-03-15 18:44:07 +0900

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,6 +34,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not_empty cookies["remember_token"]
+  end
+
+  test "login without remembering" do
+    #クッキーを保存してログイン
+    log_in_as(@user, remember_me: "1")
+    delete logout_path
+    #クッキーを削除してログイン
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies['remember_token']
+  end
 end
 
   
