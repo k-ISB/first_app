cbc6eb10ccd7497155e045e29fedc1083b27db69
k-ISB <egoukister1004@outlook.com>
2019-04-29 00:40:09 +0900

    add gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   def gravatar_for(user, options = { size: 80 })
     gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&default=retro"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
